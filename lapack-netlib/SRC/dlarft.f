fd89e78ff3d963dbd99c5055d253f3cadbc5d6e3
Martin Kroeker <martin@ruby.chemie.uni-freiburg.de>
2026-06-27 20:07:45 +0200

    Add non-trivial terminating case for LARFT (Reference-LAPACK PR 1163)

--- lapack-netlib/SRC/dlarft.f
+++ lapack-netlib/SRC/dlarft.f
@@ -5,7 +5,6 @@
 * Online html documentation available at
 *            http://www.netlib.org/lapack/explore-html/
 *
-*> \htmlonly
 *> Download DLARFT + dependencies
 *> <a href="http://www.netlib.org/cgi-bin/netlibfiles.tgz?format=tgz&filename=/lapack/lapack_routine/dlarft.f">
 *> [TGZ]</a>
@@ -13,7 +12,6 @@
 *> [ZIP]</a>
 *> <a href="http://www.netlib.org/cgi-bin/netlibfiles.txt?format=txt&filename=/lapack/lapack_routine/dlarft.f">
 *> [TXT]</a>
-*> \endhtmlonly
 *
 *  Definition:
 *  ===========
@@ -161,6 +159,7 @@
 *  =====================================================================
       RECURSIVE SUBROUTINE DLARFT( DIRECT, STOREV, N, K, V, LDV,
      $                             TAU, T, LDT )
+      IMPLICIT NONE
 *
 *  -- LAPACK auxiliary routine --
 *  -- LAPACK is a software package provided by Univ. of Tennessee,    --
@@ -183,7 +182,7 @@
 *
 *     .. Local Scalars ..
 *
-      INTEGER           I,J,L
+      INTEGER           I,J,L,NX
       LOGICAL           QR,LQ,QL,DIRF,COLV
 *
 *     .. External Subroutines ..
@@ -193,7 +192,8 @@
 *     .. External Functions..
 *
       LOGICAL           LSAME
-      EXTERNAL          LSAME
+      INTEGER           ILAENV
+      EXTERNAL          LSAME, ILAENV
 *     
 *     The general scheme used is inspired by the approach inside DGEQRT3
 *     which was (at the time of writing this code):
@@ -212,6 +212,14 @@
 *
       IF(N.EQ.1.OR.K.EQ.1) THEN
          T(1,1) = TAU(1)
+         RETURN
+      END IF
+*
+*     Determine when to cross over into the level 2 based implementation
+*
+      NX = ILAENV(3, "DLARFT", DIRECT // STOREV, N, K, -1, -1)
+      IF(K.LT.NX) THEN
+         CALL DLARFT_LVL2(DIRECT, STOREV, N, K, V, LDV, TAU, T, LDT)
          RETURN
       END IF
 *
